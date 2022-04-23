8bfd524286257fe31aa59d22d3c9973c21e2fde4
Xuejing Luo <xueluo@eu-login-01.euler.ethz.ch>
2022-04-23 19:58:40 +0200

    From -6 to -7

--- process_test_partial.sh
+++ process_test_partial.sh
@@ -1,3 +1,6 @@
+conda activate if-net
+python data_processing/convert_to_scaled_off.py -data train_gt
+python data_processing/convert_to_scaled_off.py -data test_gt
 python data_processing/convert_to_scaled_off.py -data test
 python data_processing/voxelize.py -res 32 -data test
 python data_processing/voxelized_pointcloud_sampling.py -res 128 -num_points 300 -data test
@@ -6,4 +9,12 @@ python data_processing/boundary_sampling.py -sigma 0.01 -data test
 python data_processing/filter_corrupted.py -file 'voxelization_32.npy' -delete -data test
 python data_processing/create_voxel_off.py -res 32 -data test
 python data_processing/create_pc_off.py -res 128 -num_points 300 -data test
+python data_processing/convert_to_scaled_off.py -data train
+python data_processing/voxelize.py -res 32 -data train
+python data_processing/voxelized_pointcloud_sampling.py -res 128 -num_points 300 -data train
+python data_processing/boundary_sampling.py -sigma 0.1 -data train
+python data_processing/boundary_sampling.py -sigma 0.01 -data train
+python data_processing/filter_corrupted.py -file 'voxelization_32.npy' -delete -data train
+python data_processing/create_voxel_off.py -res 32 -data train
+python data_processing/create_pc_off.py -res 128 -num_points 300 -data train
 
---
+++ train.sh
@@ -0,0 +1,1 @@
+python train.py -std_dev 0.1 0.01 -res 32 -m ShapeNet32Vox -batch_size 6
